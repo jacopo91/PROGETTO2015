037fb9b9026bd710ef93983657fe03bf026baf08
jacopo <longo.jacopo@hotmail.it>
2015-05-08 10:25:38 +0200

    classe customer

---
+++ src/model/Customer.java
@@ -0,0 +1,131 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false)
+	private String firstName;
+	@Column(nullable = false)
+	private String lastName;
+	@Column(nullable = false, unique = true)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	@Temporal(TemporalType.DATE)
+	private Date dateOfBirth;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dateOfRegistration;
+	@Column(nullable = false)
+	private boolean confermato;//l'amministratore conferma il cliente una volta ricevuta la richiesta di registrazione 
+	@OneToOne (cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+	private Address address;
+	@OneToMany(mappedBy = "customer",fetch = FetchType.EAGER)
+    private List<Order> orders;
+	
+	public Customer(String firstName, String lastName, String email, String password, Date dateOfBirth, Address address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.dateOfBirth = dateOfBirth;
+		this.dateOfRegistration = new Date();
+		this.orders = new ArrayList<Order>();
+		this.address = address;
+		this.confermato = false;
+	}
+	
+	public Customer() {
+	}
+	
+	public boolean verificaPassword(String password) {
+		return this.password.equals(password);
+	}
+	public void addOrder(Order order) {
+		this.orders.add(order);
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getDateOfRegistration() {
+		return dateOfRegistration;
+	}
+
+	public void setDateOfRegistration(Date dateOfRegistration) {
+		this.dateOfRegistration = dateOfRegistration;
+	}
+
+	public boolean isConfermato() {
+		return confermato;
+	}
+
+	public void setConfermato(boolean confermato) {
+		this.confermato = confermato;
+	}
+}
