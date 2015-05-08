03f69aadb94e679c9173246b2344c55ed26de84b
jacopo <longo.jacopo@hotmail.it>
2015-05-08 10:38:00 +0200

    classe orderLine

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -11,10 +11,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
---
+++ src/model/OrderLine.java
@@ -0,0 +1,59 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "order_line")
+public class OrderLine {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column (nullable = false)
+	private int quantity;
+	@Column (nullable = false)
+	private Float unitPrice;
+	@OneToOne
+	private Product product;
+
+	public OrderLine (Product prodotto, int quantity, Float unitPrice) {
+		this.product = prodotto;
+		this.quantity = quantity;
+		this.unitPrice = unitPrice;
+	}
+
+	public OrderLine() {
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public Float getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(Float price) {
+		this.unitPrice = price;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
+
